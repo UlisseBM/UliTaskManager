09da2336e56baffffc983841a3c032abb0abc595
Ulisse <ulissebuccella@gmail.com>
2024-11-13 22:17:54 +0100

    Update 0.0.1

---
+++ src/main/java/buccella/ulisse/Task.java
@@ -0,0 +1,49 @@
+package buccella.ulisse;
+
+/**
+ * Modelo de datos.
+ * Esta clase representa una tarea individual.
+ *
+ */
+
+public class Task {
+    private String name;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    // SETTERS Y GETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return (isCompleted ? "[X] " : "[ ] ") + name + ": " + description;
+    }
+}
---
+++ src/main/java/buccella/ulisse/TaskManager.java
@@ -0,0 +1,50 @@
+package buccella.ulisse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controlador.
+ * Maneja la logica de agregar, editar, eliminar tareas.
+ *
+ */
+
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
+
+    public List<Task> getCompletedTasks() {
+        List<Task> completedTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isCompleted()) {
+                completedTasks.add(task);
+            }
+        }
+        return completedTasks;
+    }
+
+    public List<Task> getPendingTasks() {
+        List<Task> pendingTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (!task.isCompleted()) {
+                pendingTasks.add(task);
+            }
+        }
+        return pendingTasks;
+    }
+}
