bc2cbc21a41c7a41fa0d71c112c7c19149b643db
Ulisse <ulissebuccella@gmail.com>
2024-11-18 22:29:45 +0100

    Update clase controladora

---
+++ src/main/java/buccella/ulisse/TaskManagerController.java
@@ -0,0 +1,13 @@
+package buccella.ulisse;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TaskManagerController {
+    
+    @FXML
+    private Button btnCrear; // Boton vinculado desde el FXML
+
+    @FXML
+    private void handleCrearButtonAction();
+}
